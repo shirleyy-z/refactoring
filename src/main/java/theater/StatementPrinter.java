c95fab18369a3b0d3b20cfda53072cd1e2d1185d
Shirley Zhang <shirleyzhang6628@gmail.com>
2025-11-21 17:07:28 -0500

    Task 2.2: Extract getVolumeCredits and inline thisAmount

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -31,24 +31,30 @@ public class StatementPrinter {
 
         for (Performance p : getInvoice().getPerformances()) {
 
-            final int thisAmount = getAmount(p, getPlay(p));
-
-            // add volume credits
-            volumeCredits += Math.max(p.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
-            // add extra credit for every five comedy attendees
-            if ("comedy".equals(getPlay(p).getType())) {
-                volumeCredits += p.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
-            }
+            volumeCredits += getVolumeCredits(p);
 
             // print line for this order
-            result.append(String.format("  %s: %s (%s seats)%n", getPlay(p).getName(), frmt.format(thisAmount
+            result.append(String.format("  %s: %s (%s seats)%n", getPlay(p).getName(),
+                    frmt.format(getAmount(p, getPlay(p))
                     / Constants.PERCENT_FACTOR), p.getAudience()));
-            totalAmount += thisAmount;
+            totalAmount += getAmount(p, getPlay(p));
         }
         result.append(String.format("Amount owed is %s%n", frmt.format(totalAmount
                 / Constants.PERCENT_FACTOR)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
         return result.toString();
+    }
+
+    private int getVolumeCredits(Performance performance) {
+        int result = 0;
+
+        // add volume credits
+        result += Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
+        // add extra credit for every five comedy attendees
+        if ("comedy".equals(getPlay(performance).getType())) {
+            result += performance.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
+        }
+        return result;
     }
 
     private Play getPlay(Performance performance) {
